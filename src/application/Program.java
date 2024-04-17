f7cd90ea0ec3a4459975c144606455f3fbdcb1b2
flcampanelli <flcampanelli@gmail.com>
2024-04-17 20:56:58 -0300

    feat(SellerDaoJDBC): implement findById method

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,25 +2,12 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(
-                21,
-                "Bob",
-                "bob@gmail.com",
-                LocalDate.now(),
-                3000.0,
-                department
-        );
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        System.out.println(department);
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void insert(Seller seller) {
 
@@ -23,7 +36,43 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(
+                    "SELECT sellers.*, departments.name as department_name "
+                    + "FROM sellers INNER JOIN departments "
+                    + "ON sellers.department_id = departments.id "
+                    + "WHERE sellers.id = ?"
+            );
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery(); // returns one line with all data
+
+            // if executeQuery returns a result
+            if (resultSet.next()){
+                Department department = new Department();
+                department.setId(resultSet.getInt("department_id"));
+                department.setName(resultSet.getString("department_name"));
+
+                Seller seller = new Seller();
+                seller.setId(resultSet.getInt("id"));
+                seller.setName(resultSet.getString("name"));
+                seller.setEmail(resultSet.getString("email"));
+                seller.setBaseSalary(resultSet.getDouble("base_salary"));
+                seller.setBirthDate(resultSet.getDate("birth_date").toLocalDate());
+
+                seller.setDepartment(department);
+                return seller;
+            }
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatment(statement);
+            DB.closeResultSet(resultSet);
+        }
     }
 
     @Override
